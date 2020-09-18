5f6c65ee0331443af66d9070bcc06ff4865826d4
Aman Rathore <amanrathod777@gmail.com>
2020-09-18 18:47:50 +0530

    Update

--- src/components/Home.js
+++ src/components/Home.js
@@ -31,33 +31,33 @@ function Home(){
 			 <div className="home_row">
 			 	<Product
 				 	id="1234342424"
-				 	title = "Smart Phone "
-				 	price = {11.96}
+				 	title = "Echo plus (2nd Gen)- Premium Sound "
+				 	price = {9999}
 				 	rating = {4}
-				 	image = "https://images.squarespace-cdn.com/content/v1/59d2bea58a02c78793a95114/1589398875141-QL8O2W7QS3B4MZPFWHJV/ke17ZwdGBToddI8pDm48kBVDUY_ojHUJPbTAKvjNhBl7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QHyNOqBUUEtDDsRWrJLTmmV5_8-bAHr7cY_ioNsJS_wbCc47fY_dUiPbsewqOAk2CqqlDyATm2OxkJ1_5B47U/image-asset.jpeg?format=300w"
+				 	image = "https://images-na.ssl-images-amazon.com/images/I/51ZiCYNeccL._SY300_.jpg"
 			 	/>
 			 	<Product
 				 	id="1234342424"
-				 	title = "Smart Phone "
-				 	price = {11.96}
-				 	rating = {5}
-				 	image = "https://images.squarespace-cdn.com/content/v1/59d2bea58a02c78793a95114/1589398875141-QL8O2W7QS3B4MZPFWHJV/ke17ZwdGBToddI8pDm48kBVDUY_ojHUJPbTAKvjNhBl7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QHyNOqBUUEtDDsRWrJLTmmV5_8-bAHr7cY_ioNsJS_wbCc47fY_dUiPbsewqOAk2CqqlDyATm2OxkJ1_5B47U/image-asset.jpeg?format=300w"
+				 	title = "Sony Camera "
+				 	 price = {14000}
+				 	rating = {4}
+				 	image = "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSv8ToexTe3NOUeewpoXUJg78guPXhjY-oxTA&usqp=CAU"
 			 	/>
 			 	<Product
 				 	id="1234342424"
-				 	title = "Smart Phone "
-				 	price = {11.96}
-				 	rating = {5}
-				 	image = "https://images.squarespace-cdn.com/content/v1/59d2bea58a02c78793a95114/1589398875141-QL8O2W7QS3B4MZPFWHJV/ke17ZwdGBToddI8pDm48kBVDUY_ojHUJPbTAKvjNhBl7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QHyNOqBUUEtDDsRWrJLTmmV5_8-bAHr7cY_ioNsJS_wbCc47fY_dUiPbsewqOAk2CqqlDyATm2OxkJ1_5B47U/image-asset.jpeg?format=300w"
+				 	title = "Nariko 2 Door Wardrobe with 2 Drawers in Wenge Finish "
+				 	price = {19000}
+				 	rating = {4}
+				 	image = "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcT5bY1c4p3QgxC8Veu7JHkkNadhf7pGn8MfGCtsR6XxKeR7dWui_Kt5spg3wPI&usqp=CAc"
 			 	/>
 			 </div>
 			 <div className="home_row">
 			 	<Product
 				 	id="1234342424"
-				 	title = "Smart Phone "
-				 	price = {11.96}
-				 	rating = {5}
-				 	image = "https://images.squarespace-cdn.com/content/v1/59d2bea58a02c78793a95114/1589398875141-QL8O2W7QS3B4MZPFWHJV/ke17ZwdGBToddI8pDm48kBVDUY_ojHUJPbTAKvjNhBl7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QHyNOqBUUEtDDsRWrJLTmmV5_8-bAHr7cY_ioNsJS_wbCc47fY_dUiPbsewqOAk2CqqlDyATm2OxkJ1_5B47U/image-asset.jpeg?format=300w"
+				 	title = "Stay entertained while you work"
+				 	price = {20000}
+				 	rating = {4}
+				 	image = "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQFm7n1tnZcgifr-4sU3mskEuiBm7dQJabsFg&usqp=CAU"
 			 	/>
 			 </div>
 		</div>
